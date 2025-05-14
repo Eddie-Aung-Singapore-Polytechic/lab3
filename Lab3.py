56ebdc108459613867f1b94e7e72375716c70771
Eddie <AUNGKAUNG.24@ichat.sp.edu.sg>
2025-05-15 03:37:39 +0800

    Modified contents of test_Lab3.py and Lab3.py to follow the requirements

--- Lab3.py
+++ Lab3.py
@@ -12,7 +12,13 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
+    # Checks each items of the array to check if it's int or not; if it isn't, ends the function
+    for i in arr_result:
+        if type(i) != int:
+            arr_result = 2
+            return 2
+
+    if n < 10 and n != 0: # <-- To make sure if the array's length is less than 10 and not empty
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -20,7 +26,7 @@ def bubble_sort(arr, sorting_order):
 
             # Last i elements are already in place
             for j in range(0, n - i - 1):
-
+            
                 if sorting_order == SORT_ASCENDING:
                     if arr_result[j] > arr_result[j + 1]:
                         arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
@@ -29,12 +35,15 @@ def bubble_sort(arr, sorting_order):
                 elif sorting_order == SORT_DESCENDING:
                     if arr_result[j] < arr_result[j + 1]:
                         arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
-
+                
                 else:
-                    # Return an empty array
+                    # <-- Return an empty array if the sorting_order isn't either 0/1 (Ascending and desc.)
                     arr_result = []
-    else:
-        arr_result = -1
+
+    elif n == 0: # <-- Returns 0 if the array is empty
+        arr_result = 0
+    else: # <-- Retruns 1 if it's >= 10 numbers 
+        arr_result = 1
 
     return arr_result
 
--- test_Lab3.py
+++ test_Lab3.py
@@ -28,3 +28,32 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+# Checks if the function returns value 1 if >= 10 numbers are entered
+# REQ-03
+def test_bubble_sort_is_input_greater_than_ten():
+    result = []
+    input_arr = [69,420,12,66,11,90,3,34,78,102,48]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == 1)
+
+# Checks if it returns 0 when there's no numbers entered
+# REQ-04
+def test_bubble_sort_no_number_entered():
+    result = []
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+    assert (result == 0)
+    
+# Checks if it returns 2 if the values are not int
+# REQ-05
+def test_bubble_sort_input_not_int():
+    result = []
+    input_arr = [69,'alpha',12,66.95959,'m','MOO',48,'meow']
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == 2)
